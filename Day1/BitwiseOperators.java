7fea9b525284a607b1b65a845f78eedd67b2cc2f
NARAYANAPURAM SAIDURGA <139451339+nsaidurga@users.noreply.github.com>
2025-08-08 21:51:12 +0530

    Add files via upload

---
+++ Day1/BitwiseOperators.java
@@ -0,0 +1,16 @@
+//write a program on Bitwise Operators.
+package Day1;
+
+public class BitwiseOperators {
+
+	public static void main(String[] args) {
+		int a = 5, b = 3;
+        System.out.println((a & b));  
+        System.out.println((a | b));  
+        System.out.println((a ^ b));  
+        System.out.println((~a));        
+        System.out.println((a << 1));
+        System.out.println((a >> 1));
+        }
+
+}
