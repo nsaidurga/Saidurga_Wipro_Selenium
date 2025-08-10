f18aba25d85bbdf458a324a652db780b0700b3fb
NARAYANAPURAM SAIDURGA <139451339+nsaidurga@users.noreply.github.com>
2025-08-10 17:49:40 +0530

    Update Program7.java

--- Day11/Program7.java
+++ Day11/Program7.java
@@ -3,13 +3,12 @@ package Day11;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.PreparedStatement;
-
+//write a program for order the students by their percentage.
 public class Program7 {
     public static void main(String[] args) {
         try (Connection con = Program1.getConnection()) {
-            String sql = "SELECT * FROM student WHERE percentage > ?";
+            String sql = "SELECT * FROM student ordered by percent asc";
             PreparedStatement pst = con.prepareStatement(sql);
-            pst.setFloat(1, 75);
             ResultSet rs = pst.executeQuery();
 
             System.out.println("RNo\tName\tAge\tPercentage");
@@ -24,3 +23,14 @@ public class Program7 {
         }
     }
 }
+/* output:
+Connection created
+RNo	 Name	        Age Percentage 
+123	 Kumar varma	    25	72.0
+12	 lucky sharma	21	80.0
+104	 Amit Verma	    22	83.0
+101	 Neeva Sharma	20	85.0
+102	 Reeva Sharma	20	89.0
+105	 sonal sharma	28	90.0
+103	 Shiva Upadhyay	20	91.0
+*/
